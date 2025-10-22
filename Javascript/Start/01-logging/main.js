420565a3e9b72789bb098f16ba51f78f02ac8cc1
Aish <mohammadafzaal1332@gmail.com>
2025-10-22 23:36:35 +0530

    mouseOver

---
+++ Javascript/Start/01-logging/main.js
@@ -0,0 +1,26 @@
+// logging is different from print
+// logging is more for professional debugging and stuffs
+// it is the act of recording things during execution
+
+
+console.log("Hello Aish");
+
+var first = "hallo";
+console.log(first);
+
+// u can use commas for multiple variables logging
+// no worries space will be added in places of commas
+var a = "yes";
+var b = "no";
+console.log("first:",a,"Second:",b);
+
+// u can use placeholders for variables
+// different types of variables have different placeholders
+
+console.log("%s, %s ",a,b);
+
+
+
+
+
+
---
+++ Javascript/Start/02-variables/main.js
@@ -0,0 +1,6 @@
+var tempInC = 100
+
+var tempInF = ((tempInC*9)/5)+32
+
+document.write("Temperature in celsius: "+ tempInC + " degrees <br>")
+document.write("Temperature in fahrenheit: "+ tempInF + " degrees <br>")
